41398c5fb71bb84872d3fbcd858f304763da65d8
rsk2327 <roshan.santhosh@gmail.com>
2020-06-23 19:15:02 -0400

    updated generateStates

--- generateStates.py
+++ generateStates.py
@@ -40,7 +40,7 @@ import marshal
 
 
 covidData = '/data1/roshansk/covid_data/'
-df = pd.read_csv(os.path.join(covidData, 'messages_cm_mar1_apr23_noRT.csv'), nrows = 500000)
+df = pd.read_csv(os.path.join(covidData, 'messages_cm_mar1_apr23_noRT.csv'), nrows = 1000000)
 df = df[['message_id','user_id','message']]
 
 model = BertForSequenceClassification.from_pretrained('/data1/roshansk/Exp1/checkpoint-141753-epoch-1', output_hidden_states= True)
@@ -49,33 +49,95 @@ tokenizer = BertTokenizer.from_pretrained('/data1/roshansk/Exp1/checkpoint-14175
 outputFolder = '/data1/roshansk/ADRModel_DataStore/'
 embeddingType = 'last4sum'
 
+# device = 'cuda:1'
 
-for i in tqdm(range(150000,300000)):
+# model = model.to(device)
+
+# startTime = time.time()
+
+# for i in tqdm(range(850000,1000000)):
             
-    if os.path.exists(os.path.join(outputFolder, str(i)+".msh")):
-        continue
+#     if os.path.exists(os.path.join(outputFolder, str(i)+".msh")):
+#         continue
 
 
-    tokens = tokenizer.encode(df.iloc[i]['message'].lower())
-    decoded = tokenizer.decode(tokens).split(" ")
-    logits, hidden_states = model(torch.Tensor(tokens).unsqueeze(0).long())
+#     tokens = tokenizer.encode(df.iloc[i]['message'].lower())
+#     decoded = tokenizer.decode(tokens).split(" ")
+#     logits, hidden_states = model(torch.Tensor(tokens).to(device).unsqueeze(0).long())
 
-    hidden_states = torch.stack(hidden_states).squeeze(1).permute(1,0,2)
-
-
-    if embeddingType == 'last4sum':
-        embedding = torch.sum(hidden_states[:,9:13,:],1)
-    elif embeddingType =='last4concat':
-        embedding = hidden_states[tokenIndex,9:13,:].reshape(-1)
-    elif embeddingType == 'secondlast':
-        embedding = hidden_states[tokenIndex,-2,:]
-    else:
-        embedding = hidden_states[tokenIndex,-1,:]
+#     hidden_states = torch.stack(hidden_states).squeeze(1).permute(1,0,2)
 
 
-    embedding = embedding.detach().cpu().numpy()
+#     if embeddingType == 'last4sum':
+#         embedding = torch.sum(hidden_states[:,9:13,:],1)
+#     elif embeddingType =='last4concat':
+#         embedding = hidden_states[tokenIndex,9:13,:].reshape(-1)
+#     elif embeddingType == 'secondlast':
+#         embedding = hidden_states[tokenIndex,-2,:]
+#     else:
+#         embedding = hidden_states[tokenIndex,-1,:]
 
-    marshal.dump(embedding.tolist(), open(os.path.join(outputFolder, str(i)+".msh"), 'wb'))
+
+#     embedding = embedding.detach().cpu().numpy()
+
+#     marshal.dump(embedding.tolist(), open(os.path.join(outputFolder, str(i)+".msh"), 'wb'))
+    
+
+# print(f"Time taken : {time.time() - startTime}")
+    
+
+#### AGG FILES ########
+    
+def aggFiles(index, numComp, df, tokenizer, inputFolder, outputFolder):
+    
+    filename = os.path.join(outputFolder, f"{index}.pkl")
+    print(filename)
+    
+    if os.path.exists(filename):
+        print("Skipping file since it exists")
+        return
+
+    IDList = []
+    tokenList = []
+    embList = []
+
+    for i in range(index*numComp, (index+1)*numComp):
+        text = df.iloc[i]['message']
+
+        tokens = tokenizer.convert_ids_to_tokens(tokenizer.encode(text))
+
+
+        emb = np.array(marshal.load(open(os.path.join(inputFolder, f"{i}.msh"),'rb' )))
+
+        IDList += [i]*len(tokens)
+        tokenList += tokens
+
+        embList.append(emb)
+
+    IDList = np.array(IDList)
+    tokenList = np.array(tokenList)
+    embList = np.concatenate(embList,axis=0)
+
+    subDict = {'id':IDList, 'token':tokenList,'emb':embList}
+        
+    pickle.dump(subDict, open(filename,'wb'))
+    
+    
+    
+numComp = 10000
+inputFolder = '/data1/roshansk/ADRModel_DataStore/'
+outputFolder = '/data2/roshansk/ADRModel_DataStore_10000/'
+
+for i in tqdm(range(90,100)):
+    
+    aggFiles(i, numComp, df, tokenizer, inputFolder, outputFolder)
+        
+    
+    
+    
+    
+    
+
     
     
     
